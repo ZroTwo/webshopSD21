f649488f75dba46d1aea0eff6db0c15350cd044d
ZroTwo <pizzaworld112@gmail.com>
2022-02-05 13:03:49 +0100

    admin page modified
    amdin opage bettered

--- adminindex.php
+++ adminindex.php
@@ -1,3 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=webshop", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+$stmt = $conn->prepare("SELECT * FROM products");
+$stmt->execute();
+
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,9 +22,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Macho Man Gym</title>
-    <link href="admincss.css" rel="stylesheet"">
+    <link href="admincss.css" rel="stylesheet"
+    ">
 </head>
 <body>
 <div class="header">
@@ -21,17 +40,19 @@
 <div class="main">
     <table class="table">
         <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+        <th>
+        <th scope="col">#</th>
+        <th scope="col">image</th>
+        <th scope="col">title</th>
+        <th scope="col">description</th>
+        <th scope="col">category</th>
+        <th scope="col">price</th>
         </tr>
         </thead>
         <tbody>
         <tr>
             <th scope="row">1</th>
-            <td>Mark</td>
+            <td> <?php echo $product['title'] ?> </td>
             <td>Otto</td>
             <td>@mdo</td>
         </tr>
