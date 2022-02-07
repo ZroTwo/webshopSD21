f18d6f7fc5cf83453547ae15dfd282d8f38e1f34
Gino-Landstede <gkamperm9970@student.landstede.nl>
2022-02-07 08:28:25 +0100

    Home page

--- index.php
+++ index.php
@@ -23,6 +23,9 @@
     <meta name="theme-color" content="#7952b3">
 
 
+
+
+
     <style>
         .bd-placeholder-img {
             font-size: 1.125rem;
@@ -47,12 +50,18 @@
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
                 </a>
-                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="#home" class="nav-link px-2 text-white">Home</a></li>
-                    <li><a href="products.php" class="nav-link px-2 text-white">Products</a></li>
-                    <li><a href="#workout" class="nav-link px-2 text-white">Workout Tips</a></li>
+                <ul id="menu" class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><img src="images/Logo.png" class="logo"</li>
+                    <li><a href="#home" class="nav-link px-2 text-white hover" >Home</a></li>
+                    <li class="dropdown">
+                        <a href="javascript:void(0)" class="dropbtn nav-link px-2 text-white hover">Products</a>
+                        <div class="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    <li><a href="#workout" class="nav-link px-2 text-white hover" >Workout Tips</a></li>
                 </ul>
 
                 <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
@@ -67,43 +76,137 @@
         </div>
     </header>
     <h1 class="h1">Macho Man Gym</h1>
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div id="carouselExampleCaptions" class="carousel slide justify-content-center" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="images/Logo.png" alt="First slide">
+                <img src="images/Logo.png" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Test Foto 1</h5>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi architecto at atque corporis dolorem doloremque dolorum error esse fuga hic impedit, in laboriosam magnam, modi perspiciatis placeat quam, reprehenderit ut?</p>
+                    <h5>First slide</h5>
+                    <p>Caption slide 1.</p>
                 </div>
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="images/logo3.png" alt="Second slide">
+                <img src="images/Logo.png" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Test Foto 2</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores dolores facilis, magni minus nesciunt, nobis nostrum possimus temporibus ullam unde velit veritatis! Cupiditate enim iusto neque sequi similique vel.</p>
+                    <h5>Second slide</h5>
+                    <p>Caption Slide 2.</p>
                 </div>
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="images/logo2.png" alt="Third slide">
+                <img src="images/Logo.png" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>...</h5>
-                    <p>...</p>
+                    <h5>Third slide</h5>
+                    <p>Caption Slide 3.</p>
                 </div>
             </div>
         </div>
-        <a class="carousel-control-prev" onclick="$('.carousel').carousel('prev');" role="button" data-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" onclick="$('.carousel').carousel('next');" role="button" data-slide="next">
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <div class="aboutus">
+        <h3 class="h3" >About us</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid asperiores consectetur consequuntur distinctio dolor eius eligendi exercitationem, facere fuga libero maxime neque quis quo repellat repudiandae tenetur, ullam voluptas!</p>
     </div>
 
-    <footer>
+    <!-- Footer -->
+    <footer class="text-center text-lg-start bg-light text-muted">
 
+
+        <!-- Section: Links  -->
+        <section class="">
+            <div class="container text-center text-md-start mt-5">
+                <!-- Grid row -->
+                <div class="row mt-3">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <!-- Content -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            <i class="fas fa-gem me-3"></i>Macho Man Gym
+                        </h6>
+                        <p>
+                            Here you can use rows and columns to organize your footer content. Lorem ipsum
+                            dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Social Media
+                        </h6>
+                        <p>
+                            <a href="#!" class="text-reset">Instagram</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Facebook</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Twitter</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Useful links
+                        </h6>
+                        <p>
+                            <a href="#!" class="text-reset">Products</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Workout tips</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">About us</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Contact
+                        </h6>
+                        <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                        <p>
+                            <i class="fas fa-envelope me-3"></i>
+                            info@example.com
+                        </p>
+                        <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                        <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
+            </div>
+        </section>
+        <!-- Section: Links  -->
+
+        <!-- Copyright -->
+        <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+            © 2022 Copyright:
+            <a class="text-reset fw-bold">Macho Man Gym</a>
+        </div>
+        <!-- Copyright -->
     </footer>
+    <!-- Footer -->
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -111,5 +214,50 @@
         $(document).ready(function() {
             $('.carousel').carousel();
         });
+    </script>
+    <script>
+        var slideIndex = 1;
+        showSlides(slideIndex);
+
+        // Next/previous controls
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        // Thumbnail image controls
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+        }
+
+        var slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}
+            slides[slideIndex-1].style.display = "block";
+            setTimeout(showSlides, 5000); // Change image every 2 seconds
+        }
     </script>
 </body>
